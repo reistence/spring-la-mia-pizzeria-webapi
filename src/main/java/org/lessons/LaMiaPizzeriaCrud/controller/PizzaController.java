40014ac74988f4849151261bc9af592ade00af2d
reistence <gvlamberti@gmail.com>
2023-03-30 16:26:17 +0200

    show page and search implemented

--- src/main/java/org/lessons/LaMiaPizzeriaCrud/controller/PizzaController.java
+++ src/main/java/org/lessons/LaMiaPizzeriaCrud/controller/PizzaController.java
@@ -3,12 +3,18 @@ package org.lessons.LaMiaPizzeriaCrud.controller;
 import org.lessons.LaMiaPizzeriaCrud.model.Pizza;
 import org.lessons.LaMiaPizzeriaCrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -23,10 +29,28 @@ public class PizzaController {
     }
 
     @GetMapping("/index")
-    public String index(Model model){
-        List<Pizza> pizzas = pizzaRepo.findAll();
+    public String index(Model model, @RequestParam(name = "q") Optional<String> keyword){
+        List<Pizza> pizzas;
+        if (keyword.isEmpty()) pizzas = pizzaRepo.findAll(Sort.by("name"));
+        else {
+            pizzas = pizzaRepo.findByNameContainingIgnoreCase(keyword.get());
+            model.addAttribute("keyword", keyword.get());
+        }
         model.addAttribute("menu", pizzas);
         return "/pizza/index";
+    }
+
+
+    @GetMapping("/{pizzaId}")
+    public String show(@PathVariable("pizzaId") Integer id, Model model){
+        Optional<Pizza> result = pizzaRepo.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("pizza", result.get());
+            return "/pizza/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza id: " + id + " not found");
+        }
+
     }
 
 
--- src/main/java/org/lessons/LaMiaPizzeriaCrud/repository/PizzaRepository.java
+++ src/main/java/org/lessons/LaMiaPizzeriaCrud/repository/PizzaRepository.java
@@ -3,5 +3,11 @@ package org.lessons.LaMiaPizzeriaCrud.repository;
 import org.lessons.LaMiaPizzeriaCrud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+
+    public List<Pizza> findByNameContainingIgnoreCase(String name);
+
+
 }
